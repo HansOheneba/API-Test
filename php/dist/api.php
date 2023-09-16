834ae15540826fd84201062ac14e00d38da688cd
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-09-16 06:08:46 +0000

    Create page addition

--- php/dist/api.php
+++ php/dist/api.php
@@ -1,34 +1,32 @@
 <?php
-$host = "localhost"; 
+$host = "localhost";
 $username = "root";
-$password = ""; 
-$database = "product"; 
+$password = "";
+$database = "product";
 
 $conn = mysqli_connect($host, $username, $password, $database);
 
 
-if($conn){
- $sql = "select * from products";
- $results = mysqli_query($conn,$sql);
- if($results){
-    header("Content-Type: JSON");
-    $i=0;
-    while($row = mysqli_fetch_assoc($results)){
-        $response[$i]['id'] = $row ['id'];
-        $response[$i]['name'] = $row ['name'];
-        $response[$i]['description'] = $row ['description'];
-        $response[$i]['price'] = $row ['price'];
-        $response[$i]['dateCreated'] = $row ['dateCreated'];
-        $response[$i]['dateModified'] = $row ['dateModified'];
+if ($conn) {
+    $sql = "select * from product";
+    $results = mysqli_query($conn, $sql);
+    if ($results) {
+        header("Content-Type: JSON");
+        $i = 0;
+        while ($row = mysqli_fetch_assoc($results)) {
+            $response[$i]['id'] = $row['id'];
+            $response[$i]['name'] = $row['name'];
+            $response[$i]['description'] = $row['description'];
+            $response[$i]['price'] = $row['price'];
+            $response[$i]['dateCreated'] = $row['dateCreated'];
+            $response[$i]['dateModified'] = $row['dateModified'];
 
-        $i++;
+            $i++;
+        }
+        echo json_encode($response, JSON_PRETTY_PRINT);
     }
-    echo json_encode($response,JSON_PRETTY_PRINT);
- }
-}
-else{
+} else {
     echo "Database connection failed";
 }
 
 $conn->close();
-?>
--- php/dist/create.php
+++ php/dist/create.php
@@ -1,17 +1,32 @@
 <?php
 $status = "failed";
 $message = "Unknown";
-if(!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['price']) && !empty($_POST['dateCreated'] && !empty($_POST['dateModified']))){
+if (!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['price']) && !empty($_POST['dateCreated'] && !empty($_POST['dateModified']))) {
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+    $dateCreated = $_POST['dateCreated'];
+    $dateModified = $_POST['dateModified'];
 
-$host = "localhost"; 
-$username = "root";
-$password = ""; 
-$database = "product"; 
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "product";
 
-$conn = mysqli_connect($host, $username, $password, $database);
-if($conn){
+    $conn = mysqli_connect($host, $username, $password, $database);
+    if ($conn) {
+        $sql = "insert into products (name, description, price,) 
+                values('" . $name . "','" . $description . "','" . $price . "')";
+        if (mysqli_query($conn, $sql)) {
+            if (mysqli_affected_rows($conn)) {
+                $status = "Success";
+                $message = "Product Added Successfully";
+            } else
+                $message = "Failed to add Product";
 
-}
-}
-else $message = "All fields are required";
-?>
+        } else
+            $message = "Failed to add Product";
+    } else
+        $message = "All fields are required";
+} else
+    $message = "All fields are required";
