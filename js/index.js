9f9fa3aa8e068592ed887f1159d581490cc0b232
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-09-16 02:50:09 +0000

    Arrangement

--- js/index.js
+++ js/index.js
@@ -1,9 +1,5 @@
-const express = require('express');
 const app = require('express')();
 const PORT = 8080;
-
-app.use(express.json()); // Middleware to parse JSON request bodies
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
@@ -17,7 +13,6 @@ app.get('/tshirt', (req, res) => {
 });
 
 app.post('/tshirt/:id', (req, res) => {
-
     const { id } = req.params;
     const { logo } = req.body;
 
