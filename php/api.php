bc8ecf80bdbd3ddcbf708f7123a17d6d8e45dbda
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-09-16 03:02:58 +0000

    first PHP test

---
+++ php/api.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost"; // Database host (usually "localhost" for local development)
+$username = "root"; // Database username
+$password = ""; // Database password
+$database = "product"; // Database name
+
+$conn = new mysqli($host, $username, $password, $database);
+
+
+if($conn){
+    echo "DB Connected";
+}
