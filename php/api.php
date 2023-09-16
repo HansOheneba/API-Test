2de5abb6a772ebb428362592c9d9a32e6c2802aa
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-09-16 03:25:12 +0000

    Call rows from db added

--- php/api.php
+++ php/api.php
@@ -8,7 +8,22 @@ $conn = new mysqli($host, $username, $password, $database);
 
 
 if($conn){
- 
+ $sql = "select * from products";
+ $results = mysqli_query($conn,$sql);
+ if($results){
+    $i=0;
+    while($row = mysqli_fetch_assoc($results)){
+        $response[$i]['id'] = $row ['id'];
+        $response[$i]['name'] = $row ['name'];
+        $response[$i]['description'] = $row ['description'];
+        $response[$i]['price'] = $row ['price'];
+        $response[$i]['dateCreated'] = $row ['dateCreated'];
+        $response[$i]['dateModified'] = $row ['dateModified'];
+
+        $i++;
+    }
+    echo json_encode($response,JSON_PRETTY_PRINT);
+ }
 }
 else{
     echo "Database connection failed";
