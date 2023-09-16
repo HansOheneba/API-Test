2aa025a2496f3ecfe385c1cefc05de4cbef01f97
Hans Opoku <88454205+HansOheneba@users.noreply.github.com>
2023-09-16 05:06:21 +0000

    get added to index

--- php/dist/create.php
+++ php/dist/create.php
@@ -1,4 +1,17 @@
 <?php
+$status = "failed";
+$message = "Unknown";
+if(!empty($_POST['name']) && !empty($_POST['description']) && !empty($_POST['price']) && !empty($_POST['dateCreated'] && !empty($_POST['dateModified']))){
 
+$host = "localhost"; 
+$username = "root";
+$password = ""; 
+$database = "product"; 
 
+$conn = mysqli_connect($host, $username, $password, $database);
+if($conn){
+
+}
+}
+else $message = "All fields are required";
 ?>
